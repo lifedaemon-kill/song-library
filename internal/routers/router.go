247e74e9b7455bec75429974e9b5a87be38d16fb
Ruslan <noname0263@gmail.com>
2024-12-07 16:08:04 +0900

    add swagger and openapi.yaml

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -2,6 +2,8 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	"github.com/swaggo/gin-swagger"
 	"net/http"
 	"song-library/internal/handlers"
 	"song-library/logger"
@@ -10,8 +12,14 @@ import (
 func NewRouter(handler handlers.Handler) *gin.Engine {
 	router := gin.Default()
 
+	// Маршрут для статического файла OpenAPI
+	router.StaticFile("/openapi.yaml", "./openapi.yaml")
+
+	// Маршрут для Swagger UI
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, ginSwagger.URL("http://localhost:8080/openapi.yaml")))
+
 	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, "pong!")
+		c.JSON(http.StatusOK, gin.H{"message": "pong!"})
 	})
 
 	//1 Получение данных библиотеки с фильтрацией по всем полям и пагинацией
